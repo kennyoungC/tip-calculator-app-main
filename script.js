a38496c463c835660c02a27879535077b21b9b01
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-03-11 23:31:05 +0200

    refactoring my code

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const numOfPple = document.querySelector(`.num-of-pple`);
 const tipAmount = document.querySelector(`.tip-amount`);
 const totalAmount = document.querySelector(`.total-amount`);
 const reset = document.querySelector(`.reset`);
-
+const errorMsg = document.querySelector(`.cant-do`);
 const gridBox = document.querySelector(`.grid-box`);
 const calcTip = function (e) {
   if (e.target.classList.contains(`grid-number`)) {
@@ -19,12 +19,16 @@ const calcTip = function (e) {
       return;
     }
     if (people <= 1 || people === "") {
-      people = 1;
-      const tip = (bill / people) * (num / 100);
-      const total = bill / people + tip;
+      errorMsg.textContent = `Can't be zero`;
+      errorMsg.style.color = `red`;
+      numOfPple.style.boxShadow = `inset 0 0 0 5px rgba(236, 16, 9, 0.5)`;
 
-      totalAmount.textContent = `$${total.toFixed(2)}`;
-      tipAmount.textContent = `$${tip.toFixed(2)}`;
+      // people = 1;
+      // const tip = (bill / people) * (num / 100);
+      // const total = bill / people + tip;
+
+      // totalAmount.textContent = `$${total.toFixed(2)}`;
+      // tipAmount.textContent = `$${tip.toFixed(2)}`;
     } else {
       const tip = (bill / people) * (num / 100);
       const total = bill / people + tip;
@@ -34,11 +38,18 @@ const calcTip = function (e) {
     }
   }
 };
-const resetAll = function () {
-  totalAmount.textContent = `$0.00`;
-  tipAmount.textContent = `$0.00`;
+const blank = function () {
   billInput.value = "";
   numOfPple.value = "";
 };
+
+const resetAll = function () {
+  numOfPple.style.boxShadow = `none`;
+  errorMsg.textContent = ``;
+  totalAmount.textContent = `$0.00`;
+  tipAmount.textContent = `$0.00`;
+  blank();
+};
+billInput.addEventListener(`click`, resetAll);
 gridBox.addEventListener(`click`, calcTip);
 reset.addEventListener(`click`, resetAll);
